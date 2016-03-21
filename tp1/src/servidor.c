869d20e4e33195740cc617d1e8d5db56ede3fb51
mporto <manu.porto94@gmail.com>
2016-03-20 22:36:23 -0300

    Servidor reimplementado con la nueva api de socket.

--- tp1/src/servidor.c
+++ tp1/src/servidor.c
@@ -8,45 +8,29 @@
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netdb.h>
-//#include<netinet/in.h>
 #include<unistd.h>
 
+
+#include "socket_api.h"
+
 int main() {
-    int status, skt, peerskt;
-    struct addrinfo hints;
-    struct addrinfo *res;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
+    sktinfo_t skt;
 
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    socket_init(&skt, "", "8080");
 
-    status = getaddrinfo(NULL, "8080", &hints, &res);
-
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        return 1;
-    }
-
-    skt = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    bind(skt, res->ai_addr, res->ai_addrlen);
-    freeaddrinfo(res);
-    listen(skt, 10);
+    socket_bind(&skt);
+    socket_listen(&skt, 10);
     
     // accept incoming connections
-    addr_size = sizeof(struct sockaddr_storage);
-    peerskt = accept(skt, (struct sockaddr *)&their_addr, &addr_size); 
+    sktinfo_t peerskt;
+    socket_accept(&skt, &peerskt); 
 
     char msg[] = "Connection made";
     int len, bytes_send;
 
     len = strlen(msg);
-    bytes_send = send(peerskt, msg, len, 0);
+    bytes_send = send(peerskt.fd, msg, len, 0);
 
-    shutdown(peerskt, SHUT_RDWR);
-    close(peerskt);
-    shutdown(skt, SHUT_RDWR);
-    close(skt);
+    socket_destroy(&skt);
+    socket_destroy(&peerskt);
 }

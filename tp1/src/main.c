16bd20fbb6f6960aea07225613112c90cdfda060
mporto <manu.porto94@gmail.com>
2016-03-22 11:36:15 -0300

    Refactor a main

--- tp1/src/main.c
+++ tp1/src/main.c
@@ -1,11 +1,12 @@
 #include<string.h>
+#include<stdlib.h>
 
 #include "cliente.h"
 #include "servidor.h"
 
 int main(int argc, char *argv[]) {
-    if (strcmp(argv[1], "cliente") == 0) cliente();
-    else servidor();
+    if (strcmp(argv[1], "client") == 0) cliente(argv[2], argv[3], argv[4], argv[5], argv[6], atoi(argv[7]));
+    else servidor(argv[2]);
     
     return 0; 
 }

cf8a6b5a3c9e24ed03db9f018128dbef2eee2b0a
mporto <manu.porto94@gmail.com>
2016-03-26 11:35:32 -0300

    Se agrega funcion de procesamiento de archivo local del servidor.

--- tp1/src/server.c
+++ tp1/src/server.c
@@ -5,10 +5,13 @@
 #include "socket_api.h"
 #include "dynamic_array.h"
 
-static void receive_file_info(sktinfo_t *skt);
+static void receive_file_info(sktinfo_t *skt, char *filename, 
+        int filename_length, int *block_size);
 
 static void receive_file_data(sktinfo_t *skt, array_t *chksm_array);
 
+static int compare_files(sktinfo_t *skt, char *filename, array_t *chksm_array,
+        int block_size);
 
 void servidor(char *port) {
     sktinfo_t skt;
@@ -20,25 +23,29 @@ void servidor(char *port) {
     
     // accept incoming connections
     socket_accept(&skt); 
+    
 
-    receive_file_info(&skt);
+    int filename_length;
+    socket_receive(&skt, &filename_length, sizeof(filename_length));   
+
+    char filename[filename_length];
+    int block_size;
+    receive_file_info(&skt, filename, filename_length, &block_size);
 
     array_t chksm_array;
     array_init(&chksm_array, 10);
     receive_file_data(&skt, &chksm_array);
+    compare_files(&skt, filename, &chksm_array, block_size);
     array_destroy(&chksm_array);
     socket_destroy(&skt);
 }
 
-static void receive_file_info(sktinfo_t *skt) {
-    int filename_length;
-    socket_receive(skt, &filename_length, sizeof(filename_length));
+static void receive_file_info(sktinfo_t *skt, char *filename, 
+        int filename_length, int *block_size) {
     //printf("%d\n", filename_length);
-    char server_file[filename_length + 1];
-    socket_receive(skt, server_file, filename_length);
+    socket_receive(skt, filename, filename_length);
     //printf("%s\n", server_file);
-    int block_size;
-    socket_receive(skt, &block_size, sizeof(block_size));
+    socket_receive(skt, block_size, sizeof(*block_size));
     //printf("%d\n", block_size);
 }
 
@@ -48,8 +55,17 @@ static void receive_file_data(sktinfo_t *skt, array_t *chksm_array) {
     socket_receive(skt, &operation, sizeof(operation));
     while (operation == 1) {
         socket_receive(skt, &checksum, sizeof(checksum));
-        printf("%x\n", checksum);
+        array_append(chksm_array, checksum);
         socket_receive(skt, &operation, sizeof(operation));
     }
 }
+static int compare_files(sktinfo_t *skt, char *filename, array_t *chksm_array,
+        int block_size) {
+    int a = 0;
+    for(int i = 0; i < chksm_array->elements; i++) {
+        a = chksm_array->data[i];
+        printf("check: %x\n", a);
+    }
 
+    return a;
+}

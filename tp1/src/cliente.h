1c25fca6a24e7f99605ea93e2455be82967aaec8
mporto <manu.porto94@gmail.com>
2016-03-25 21:50:01 -0300

    Se cambian los nombres a ingles

--- tp1/src/cliente.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOCKET_API_H_
-#define SOCKET_API_H_
-
-#include<stdio.h>
-#include<string.h>
-
-void cliente(char *hostname, char *port, char *old_local_file, char *new_local_file, char *new_remote_file, int block_size);
-#endif
--- tp1/src/servidor.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVIDOR_H_
-#define SERVIDOR_H_
-
-void servidor(char *port);
-
-#endif

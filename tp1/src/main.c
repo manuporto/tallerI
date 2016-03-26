8ae99357666a5c7b0ff33cc4202e5f19f6367006
mporto <manu.porto94@gmail.com>
2016-03-26 16:45:55 -0300

    Cpplint fixes

--- tp1/src/main.c
+++ tp1/src/main.c
@@ -5,9 +5,11 @@
 #include "server.h"
 
 int main(int argc, char *argv[]) {
-    if (strcmp(argv[1], "client") == 0) cliente(argv[2], argv[3], argv[4], 
-            argv[5], argv[6], atoi(argv[7]));
-    else servidor(argv[2]);
+    if (strcmp(argv[1], "client") == 0) {
+        cliente(argv[2], argv[3], argv[4], argv[5], argv[6], atoi(argv[7]));
+    } else {
+        servidor(argv[2]);
+    }
     
     return 0; 
 }

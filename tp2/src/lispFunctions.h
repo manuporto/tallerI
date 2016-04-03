f2cb61b82699b0f97e019547d4b50bff3689ddde
mporto <manu.porto94@gmail.com>
2016-04-03 00:45:03 -0300

    Se agregan todas las operaciones aritmeticas

--- tp2/src/lispFunctions.h
+++ tp2/src/lispFunctions.h
@@ -19,10 +19,18 @@
 #include <vector>
 
 using namespace std;
+enum LispFunctionType {
+    add,
+    sub,
+    mul,
+    div
+};
 
 class LispFunction {
     public:
-        virtual void run(const vector<int>& args, int& result) = 0;
+        virtual void run(const vector<int>& args, int& result){
+            throw "Not Implemented.";
+        }
         virtual ~LispFunction() {}
 };
 
@@ -35,24 +43,64 @@ class LispArithmeticFunction: public LispFunction {
 class LispAdd: public LispArithmeticFunction {
     public:
         virtual void run(const vector<int>& args, int& result) {
-           for (size_t i = 0; i < args.size(); i++) {
+            for (size_t i = 0; i < args.size(); i++) {
                result += args[i];
-           } 
+            } 
         }
 
         virtual ~LispAdd() {}
 };
 
+class LispSub: public LispArithmeticFunction {
+    public:
+        virtual void run(const vector<int>& args, int& result) {
+            for (size_t i = 0; i < args.size(); i++) {
+               result -= args[i];
+            } 
+        }
+
+        virtual ~LispSub() {}
+};
+
+class LispMul: public LispArithmeticFunction {
+    public:
+        virtual void run(const vector<int>& args, int& result) {
+            result = args[0];
+            for (size_t i = 1; i < args.size(); i++) {
+               result *= args[i];
+            } 
+        }
+
+        virtual ~LispMul() {}
+};
+
+class LispDiv: public LispArithmeticFunction {
+    public:
+        virtual void run(const vector<int>& args, int& result) {
+            result = args[0];
+            for (size_t i = 1; i < args.size(); i++) {
+               result /= args[i];
+            } 
+        }
+
+        virtual ~LispDiv() {}
+};
+
 class LispFunctionFactory {
     public:
-        enum LispFunctionType {
-            add
-        };
-
-        static LispFunction *newLispFunction(LispFunctionType type) {
+        static LispFunction* newLispFunction(LispFunctionType type) {
             switch (type) {
                 case add: 
                     return new LispAdd;
+                    break;
+                case sub: 
+                    return new LispSub;
+                    break;
+                case mul: 
+                    return new LispMul;
+                    break;
+               case div: 
+                    return new LispDiv;
                     break;
                 default:
                     throw "Invalid LispFunctionType.";

7f5ea105f452a8de67ec0590066399d429ec10eb
mporto <manu.porto94@gmail.com>
2016-04-07 23:06:16 -0300

    Se crea clase parser que encapsula logica de parseo y ejecucion de instrucciones

---
+++ tp2/src/parser.h
@@ -0,0 +1,83 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  parser.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/04/16 21:09:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * ============================================================================
+ */
+
+#include <queue>
+
+#include "lispFunctions.h"
+#include "enviroment.h"
+
+class Parser {
+    private:
+        LispFunctionFactory funFactory;
+
+        void replace_substr(string &input, const string old_str, 
+                const string new_str) {
+            size_t index = 0;
+            while ( (index = input.find(old_str, index))!= string::npos ) {
+                input.replace(index, old_str.size(), new_str);
+                index += new_str.size();
+            }
+        }
+
+        queue<string> separate_tokens(string input) {
+            replace_substr(input, "(", " ( "); 
+            replace_substr(input, ")", " ) "); 
+            queue<string> parsed;
+            stringstream ss(input);
+            string sub;
+
+            while (ss >> sub) {
+                parsed.push(sub) ;
+            }
+            return parsed;
+        }
+        string eval(Functions &funs, queue<string> &parsed) {
+            vector<string> args;
+            LispFunction *l = funFactory.newLispFunction(dummy);
+            string element, res;
+            while (!parsed.empty()) {
+                element = parsed.front();
+                parsed.pop();
+                if (element.compare(")") == 0) {
+                    res = l->run(args);
+                    return res; 
+                } else if (element.compare("(") == 0) {
+                    cout << "eval call" << endl;
+                    res = eval(funs, parsed);
+                    args.push_back(res);
+                } else if (funs.count(element)) {
+                    cout << "lisp fun def" << endl;
+                    l = funFactory.newLispFunction(funs[element]);
+                } else {
+                    cout << "arg push" << endl;
+                    args.push_back(element);
+                }
+            }
+
+                return res;
+        }
+    public:
+        Parser() {
+            funFactory = LispFunctionFactory();
+        }
+
+        string run(Functions &funs, string input) {
+            queue<string> parsed = separate_tokens(input);
+            return eval(funs, parsed);
+        }
+};

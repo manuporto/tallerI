c307f0df13e50bcbb7f4ac6372d20cb81ec0b04d
mporto <manu.porto94@gmail.com>
2016-04-17 23:22:59 -0300

    Se comienza a reimplementar el socket

--- tp3/src/socket2.cpp
+++ tp3/src/socket2.cpp
@@ -1,10 +1,66 @@
+#include <errno>
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+
 #include "socket2.h"
 
-socket2::socket2()
-{
+socket2::socket2(string hostname, string port, addrinfo& res) {
+    int status;
+    addrinfo hints;
+
+    memset(&hints, 0, sizeof(addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (!hostname) {
+        hints.ai_flags = AI_PASSIVE;
+    }
+
+    status = getaddrinfo(hostname.c_str(), port.c_str(), &hints, res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    }
+
+    fd = socket(res.ai_family, res.ai_socktype, res.ai_protocol);
+    if (self->fd == -1) {
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+    }
 }
 
-socket2::~socket2()
-{
+void socket2::socket_bind(addrinfo& res) {
+    int status = bind(fd, res.ai_addr, res.ai_addrlen);
+    if (status == -1) {
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+    }
 }
 
+void socket2::socket_connect(addrinfo& res) {
+    int status = connect(fd, res.ai_addr, res.ai_addrlen);
+    if (status == -1) {
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+    }
+}
+
+void socket2::socket_listen(int backlog) {
+    int status = listen(fd, backlog);
+    if (status == -1) {
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+    }
+}
+
+void socket2::socket_accept() {}
+
+void socket2::socket_send(void* buf, int size) {}
+
+void socket2::socket_receive(void* buf, int size) {}
+
+void socket2::process_message(void* buf, int size, int mode) {}
+
+socket2::~socket2() {
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
+}
--- tp3/src/socket2.h
+++ tp3/src/socket2.h
@@ -1,12 +1,22 @@
 #ifndef SOCKET2_H
 #define SOCKET2_H
 
-class socket2
-{
-public:
-    socket2();
+typedef struct addrinfo addrinfo;
+
+class socket2 {
+   public:
+    socket2(string hostname, string port, addrinfo& res);
+    void socket_bind(addrinfo& res);
+    void socket_connect(addrinfo& res);
+    void socket_listen(int backlog);
+    void socket_accept();
+    void socket_send(void* buf, int size);
+    void socket_receive(void* buf, int size);
     ~socket2();
 
+   private:
+    void process_message(void* buf, int size, int mode);
+    int fd;
 };
 
-#endif // SOCKET2_H
+#endif  // SOCKET2_H

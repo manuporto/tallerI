77b8310a25df5337c3b0b5366648bf73a5188fde
manuporto <manu.porto94@gmail.com>
2016-07-10 20:02:07 -0300

    aaa

--- finales/ej520061212.c
+++ finales/ej520061212.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #define FILE_NAME "a.txt"
 
 enum states {INIT, OP_PARENTHESIS, SLASH};
 
+int read_char(FILE *fp, size_t *index) {
+    fseek(fp, *index, SEEK_SET);
+    int c = fgetc(fp);
+    *index = ftell(fp);
+    return c;
+}
+
 void write(FILE *fp, int c, size_t *index) {
+    fseek(fp, *index, SEEK_SET);
     fputc(c, fp);
     *index = ftell(fp);
 }
@@ -13,21 +23,20 @@ void write(FILE *fp, int c, size_t *index) {
 int main() {
     FILE *fp = fopen(FILE_NAME, "rb+");
     int c;
+    int file_length = 0;
     int state = INIT;
     size_t read_index = ftell(fp);
     size_t write_index = read_index;
 
-    while((c = fgetc(fp)) != EOF) {
-        read_index = ftell(fp);
+    while((c = read_char(fp, &read_index)) != EOF) {
+        ++file_length;
         switch (state) {
             case INIT:
-                if (c == '(') {
-                    state = OP_PARENTHESIS;
-                    write(fp, c, &write_index);
-                } else if (c == '/') {
+                if (c == '/') {
                     state = SLASH;
                 } else {
                     write(fp, c, &write_index);
+                    if (c == '(') state = OP_PARENTHESIS;
                 }
                 break;
             case OP_PARENTHESIS:
@@ -36,14 +45,19 @@ int main() {
                 break;
             case SLASH:
                 if (c == '/') {
-                    fseek(fp, write_index, SEEK_SET);
+                    --file_length;
+                    write(fp, '*', &write_index);
+                    state = INIT; 
                 }
                 else if (c == '(') state = OP_PARENTHESIS;
                 else state = INIT;
+                write(fp, '/', &write_index);
                 write(fp, c, &write_index);
                 break;
         }
     }
 
+    fclose(fp);
+    truncate(FILE_NAME, file_length);
     return 0;
 }

c27576dfb68268659985c5fe2bc1f208273c3085
mporto <manu.porto94@gmail.com>
2016-07-11 11:41:26 -0300

    Correccion ejercicio 5 20061212

--- finales/ej520061212.c
+++ finales/ej520061212.c
@@ -14,7 +14,7 @@ int read_char(FILE *fp, size_t *index) {
     return c;
 }
 
-void write(FILE *fp, int c, size_t *index) {
+void write_char(FILE *fp, int c, size_t *index) {
     fseek(fp, *index, SEEK_SET);
     fputc(c, fp);
     *index = ftell(fp);
@@ -29,30 +29,31 @@ int main() {
     size_t write_index = read_index;
 
     while((c = read_char(fp, &read_index)) != EOF) {
+        printf("rd: %zu, w: %zu\n", read_index, write_index);
         ++file_length;
         switch (state) {
             case INIT:
                 if (c == '/') {
                     state = SLASH;
                 } else {
-                    write(fp, c, &write_index);
+                    write_char(fp, c, &write_index);
                     if (c == '(') state = OP_PARENTHESIS;
                 }
                 break;
             case OP_PARENTHESIS:
-                if (c == ')') state = INIT;
-                write(fp, c, &write_index);
+                if (c == ')') {state = INIT;}
+                write_char(fp, c, &write_index);
                 break;
             case SLASH:
                 if (c == '/') {
                     --file_length;
-                    write(fp, '*', &write_index);
+                    write_char(fp, '*', &write_index);
                     state = INIT; 
+                } else{
+                    state = c == '(' ? OP_PARENTHESIS : INIT;
+                    write_char(fp, '/', &write_index);
+                    write_char(fp, c, &write_index);
                 }
-                else if (c == '(') state = OP_PARENTHESIS;
-                else state = INIT;
-                write(fp, '/', &write_index);
-                write(fp, c, &write_index);
                 break;
         }
     }
